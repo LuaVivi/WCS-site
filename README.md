be1f3b81d5b34773f4186dbb31f0b632f7764db5
LuaVivi <viviana.pasaniuc6@gmail.com>
2025-07-27 17:49:55 +0300

    mesaj

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# WCS-site" 
+"Acesta este site-ul web al clubului de anima" 
+"Acesta este site-ul web al clubului de anima?ie Wax Cherry Studio." 
---
+++ funct.js
@@ -0,0 +1,9 @@
+document.getElementById("ilink").onclick = function () {
+            window.location.href = "Inscriere.html";
+};
+
+document.getElementById("home").onclick = function () {
+            window.location.href = "WaxCherryStudio-Home.html";
+};
+
+
