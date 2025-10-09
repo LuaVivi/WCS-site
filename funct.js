13633eaf2f9ce60f06baf90b57e6bf48fd5a8d1d
LuaVivi <142715887+LuaVivi@users.noreply.github.com>
2025-10-09 11:04:35 +0300

    Update funct.js

--- funct.js
+++ funct.js
@@ -2,22 +2,10 @@ document.getElementById("ilink").onclick = function () {
             window.location.href = "Inscriere.html";
 };
 
-document.getElementById("insta").onclick = function () {
-            window.open("https://www.instagram.com/studioul_corcoduselor/");
-};
-
-document.getElementById("tiktok").onclick = function () {
-            window.open("https://www.tiktok.com/@wax_cherry_studio?lang=en-GB");
-};
-
 document.getElementById("home").onclick = function () {
             window.location.href = "index.html";
 };
 
 document.getElementById("OFPT").onclick = function () {
             window.location.href = "OFeliePentruToti.html";
-};
-
-document.getElementById("yt").onclick = function () {
-            window.open("https://www.youtube.com/@Wax_CherryStudio");
 };
