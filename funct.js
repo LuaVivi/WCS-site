25f111177fec21b86eaa11013378be5c3bd6bf43
LuaVivi <142715887+LuaVivi@users.noreply.github.com>
2025-10-09 11:01:14 +0300

    Update funct.js

--- funct.js
+++ funct.js
@@ -2,6 +2,14 @@ document.getElementById("ilink").onclick = function () {
             window.location.href = "Inscriere.html";
 };
 
+document.getElementById("insta").onclick = function () {
+            window.open("https://www.instagram.com/studioul_corcoduselor/");
+};
+
+document.getElementById("tiktok").onclick = function () {
+            window.open("https://www.tiktok.com/@wax_cherry_studio?lang=en-GB");
+};
+
 document.getElementById("home").onclick = function () {
             window.location.href = "index.html";
 };
@@ -12,12 +20,4 @@ document.getElementById("OFPT").onclick = function () {
 
 document.getElementById("yt").onclick = function () {
             window.open("https://www.youtube.com/@Wax_CherryStudio");
-};
-
-document.getElementById("insta").onclick = function () {
-            window.open("https://www.instagram.com/studioul_corcoduselor/");
-};
-
-document.getElementById("tiktok").onclick = function () {
-            window.open("https://www.tiktok.com/@wax_cherry_studio?lang=en-GB");
 };
